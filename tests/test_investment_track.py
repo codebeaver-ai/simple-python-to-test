751b5593fd480906f3c3bb28de3fa36a615b8b2b
Vittorio Banfi <vittorio.banfi@gmail.com>
2024-12-24 11:11:36 +0100

    fix test import

--- tests/test_investment_track.py
+++ tests/test_investment_track.py
@@ -3,6 +3,6 @@ from datetime import datetime
 from investment_tracker import InvestmentTracker
 
 
-class TestInvestmentTracker(pytest.TestCase):
+class TestInvestmentTracker:
     def test_record_transaction(self):
         assert InvestmentTracker().record_transaction(100, "food", "groceries") == True
